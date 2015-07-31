05b19e15d7291aae3d3566047f22077ed97dd0a3
duhone <duhone@conjuredrealms.com>
2015-07-30 22:50:12 -0700

    minor

--- inc/Platform/Process.h
+++ inc/Platform/Process.h
@@ -8,9 +8,9 @@ namespace CR
 	{
 		struct IProcess
 		{
+			virtual ~IProcess() = default;
 			//Returns false if timed out waiting for process to close, or if some other error occured
 			virtual bool WaitForClose(const std::chrono::milliseconds& a_maxWait) = 0;
-			virtual ~IProcess() = default;
 		};
 
 		std::unique_ptr<IProcess> CRCreateProcess(const char* a_executablePath, const char* a_commandLine);
